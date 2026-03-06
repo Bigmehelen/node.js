042d15d6063fa75599ff557b01692c1ceb2c0365
Helen <getfitbyhelena@gmail.com>
2026-03-06 14:57:07 +0100

    updating route

--- url-shortner/routes/UserRoutes.js
+++ url-shortner/routes/UserRoutes.js
@@ -3,7 +3,7 @@ const router=express.Router()
 import UserController from "../controllers/UserController.js";
 import {register} from "../controllers/UserController.js";
 
-router.post("/register", register)
-router.post("/login", UserController.login)
+router.post("api/register", register)
+router.post("api/login", UserController.login)
 
 export default router;
